8018f9a1e55bddfa41bbb2414a71a372e40e6d0f
Borrisson <lucborris@gmail.com>
2021-01-18 23:50:13 +0000

    Eslint Style Fix

--- express_server.js
+++ express_server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 
-function generateRandomString() {
+const generateRandomString = function() {
   return Math.random().toString(36).substring(2, 8);
-}
+};
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -25,21 +25,21 @@ app.get("/urls", (req, res) => {
 });
 
 //creates unique ID for urlDB
-app.post("/urls", (req, res) => { 
+app.post("/urls", (req, res) => {
   let id = '';
   do {
     id = generateRandomString();
-  } while(urlDatabase[id]);
+  } while (urlDatabase[id]);
   urlDatabase[id] = req.body.longURL;
-  res.redirect(`/urls/${id}`)   
+  res.redirect(`/urls/${id}`);
 });
 
 //redirects from shortURL if it exists in DB
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  if(!longURL) {
+  if (!longURL) {
     res.redirect('/404');
-  };
+  }
   res.redirect(longURL);
 });
 
@@ -49,9 +49,9 @@ app.get("/urls/new", (req, res) => {
 
 //redirects from shortURL if it exists in DB
 app.get("/urls/:shortURL", (req, res) => {
-  if(!urlDatabase[req.params.shortURL]) {
+  if (!urlDatabase[req.params.shortURL]) {
     res.redirect('/404');
-  };
+  }
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
@@ -61,7 +61,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get('/404', (req, res) => {
-  res.send(`404 Page Not Found`)
+  res.send(`404 Page Not Found`);
 });
 
 app.get('*', (req, res) => {
