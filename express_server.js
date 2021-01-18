4c62d526b6ba96a327cda623d241255b0fa06ff2
Borrisson <lucborris@gmail.com>
2021-01-18 18:45:59 +0000

    Added urls shortform page with EJS

--- express_server.js
+++ express_server.js
@@ -16,7 +16,13 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
-})
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  console.log(req.params);
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
