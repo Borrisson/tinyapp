500d661cec4740c9f3b9ba62bbe9379d019f7299
Borrisson <lucborris@gmail.com>
2021-01-21 12:23:43 +0000

    Made some param names more descriptive

--- public/helpers/userAuthenticator.js
+++ public/helpers/userAuthenticator.js
@@ -5,24 +5,24 @@ const generateRandomString = function () {
   return Math.random().toString(36).substring(2, 8);
 };
 
-//checks if email exists, third optional param(is for id lookup)
-const emailAuth = function (users, body, { lookupID = false, registration = false } = {}) {
-  for (let [id, { email, password }] of Object.entries(users)) {
-    if (email === body.email && bcrypt.compareSync(body.password, password) && lookupID) {
+//checks if email exists, third optional param(is for id lookup, or for registration check)
+const emailAuth = function (database, user, { lookupID = false, registration = false } = {}) {
+  for (let [id, { email, password }] of Object.entries(database)) {
+    if (email === user.email && bcrypt.compareSync(user.password, password) && lookupID) {
       return id;
       //for login, must match
-    } else if (email === body.email && bcrypt.compareSync(body.password, password)) {
+    } else if (email === user.email && bcrypt.compareSync(user.password, password)) {
       return true;
       //for registration, if email exists don't create another
-    } else if (email === body.email && registration) {
+    } else if (email === user.email && registration) {
       return true;
     }
   }
   return false;
 };
 
-const locateID = function (users, body) {
-  return emailAuth(users, body, { lookupID: true });
+const locateID = function (database, userInfo) {
+  return emailAuth(database, userInfo, { lookupID: true });
 };
 
 const loggedIn = function (userId) {
